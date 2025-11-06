36ee9767cace2f63494bd5fa5d276d743f244fd3
Gracey19 <Gracey19@users.noreply.github.com>
2025-11-06 03:37:11 -0600

    Task 1: Modified JSX, added WelcomeMessage component, and updated README

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome kids, I am learning React in ALX FE program</h1>
+      <p>Currently learning about JSX!</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
+
+
