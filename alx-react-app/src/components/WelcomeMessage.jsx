d8f8c221b4a35a5d8f5a34a05c84d953e2b38236
Gracey19 <Gracey19@users.noreply.github.com>
2025-11-06 03:44:21 -0600

    Fix: corrected h1 text to match checker requirement

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,12 +1,11 @@
 function WelcomeMessage() {
   return (
     <div>
-      <h1>Welcome kids, I am learning React in ALX FE program</h1>
-      <p>Currently learning about JSX!</p>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
     </div>
   );
 }
 
 export default WelcomeMessage;
-
 
