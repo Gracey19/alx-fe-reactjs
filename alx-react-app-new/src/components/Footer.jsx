062d19e3984cb8b79c78102ad15632bb64bd9783
Gracey19 <Gracey19@users.noreply.github.com>
2025-11-07 04:37:16 -0600

    Fix: added inline style to Footer for ALX checker

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer style={{ backgroundColor: '#36454F', color: 'white', textAlign: 'center', padding: '12px', marginTop: '20px' }}>
+      <p>&copy; 2025 Grace Wesonga. All rights reserved.</p>
     </footer>
   );
 }
