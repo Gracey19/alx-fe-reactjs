ecd8594d6c36fb1c1682b60c1314bdd47b05d3e9
Gracey19 <Gracey19@users.noreply.github.com>
2025-11-13 05:54:29 -0600

    Task 2:Added recipe details, edit, and delete features

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
-import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
 function RecipeList() {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -7,7 +8,10 @@ function RecipeList() {
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
+
           <p>{recipe.description}</p>
         </div>
       ))}
