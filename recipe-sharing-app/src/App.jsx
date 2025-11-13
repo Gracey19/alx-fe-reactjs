a250f5785a648701139c807434faf6b978a4f04e
Gracey19 <Gracey19@users.noreply.github.com>
2025-11-13 05:11:57 -0600

    Intial recipe app with Zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
+
